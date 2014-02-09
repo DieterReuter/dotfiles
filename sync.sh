08e0667945d6c9da7bfb5709eef85f1d5be826ee
Dieter Reuter <dieter.reuter@me.com>
2014-02-09 18:36:45 +0100

    Include VIM color schemes

---
+++ sync.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+git pull
+function doIt() {
+	# prepare all dirs and installs
+        if [ ! -d ~/code ]; then
+          mkdir ~/code
+        fi
+        if [ ! -d ~/code/z ]; then
+          git clone https://github.com/rupa/z.git ~/code/z
+        fi
+        if [ ! -d ~/.vim/bundle/vundle ]; then
+          git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+        fi
+
+	# sync all files to out $HOME directory ~/
+	rsync \
+	--exclude ".git/" \
+	--exclude ".gitignore" \
+	--exclude ".gitkeep" \
+	--exclude ".DS_Store" \
+	--exclude "sync.sh" \
+	--exclude "README.md" \
+	--exclude "LICENSE" \
+	-av . ~
+
+	# Vundle install all defined Bundles
+        vim +BundleInstall +qall
+}
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	doIt
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt
+	fi
+fi
+unset doIt
+source ~/.bash_profile
